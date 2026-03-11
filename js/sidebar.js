d2bae5fb4ae9f64d8668b307c789e3cc305e52cc
Manuell00 <manuell.caselli@gmail.com>
2026-03-11 18:48:21 +0100

    modifiche sidebar e margin-padding

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,12 +1,6 @@
 const toggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("mobile-menu");
+const mobileMenu = document.getElementById("mobile-menu");
 
 toggle.addEventListener("click", () => {
-    menu.classList.toggle("open");
-});
-
-document.querySelectorAll(".mobile-menu a").forEach(link => {
-    link.addEventListener("click", () => {
-        menu.classList.remove("open");
-    });
+    mobileMenu.classList.toggle("open");
 });
